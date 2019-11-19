3fe6112602af41b58c48b4d4edc422f8b42221b8
mert2000 <mert.temel12@gmail.com>
2019-11-19 13:25:10 +0300

    Java_poject

---
+++ day2/src/A1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class A1 {
+    public static void main (String args[]) {
+        int a;
+        System.out.println("Write a number:");
+        Scanner sc = new Scanner(System.in);
+        a=Integer.parseInt(sc.nextLine());
+        if(a>0)
+        {
+            System.out.println("The number"+a+"is positive");
+        }
+        else
+            {
+                System.out.println("The number"+a+" is negative");
+            }
+
+    }
+}
+
